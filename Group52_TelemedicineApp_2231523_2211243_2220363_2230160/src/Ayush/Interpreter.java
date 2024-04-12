a56579de4ae8f7c1be8c53eebdd76952966682dc
Ayush Das <dasayush953@gmail.com>
2024-04-13 02:18:41 +0600

    code add
    interpreter profile added and nurse training code added

---
+++ Group52_TelemedicineApp_2231523_2211243_2220363_2230160/src/Ayush/Interpreter.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ayush;
+
+/**
+ *
+ * @author dasay
+ */
+public class Interpreter extends User{
+    protected String email;
+    protected String gender;
+    protected int phoneNumber;
+    protected String dob;
+
+    public Interpreter(String email, String gender, int phoneNumber, String name, int id, String category, String password) {
+        super(name, id, category, password);
+        this.email = email;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Interpreter{" + "email=" + email + ", gender=" + gender + ", phoneNumber=" + phoneNumber + ", dob=" + dob + '}';
+    }
+    
+    
+    
+}
