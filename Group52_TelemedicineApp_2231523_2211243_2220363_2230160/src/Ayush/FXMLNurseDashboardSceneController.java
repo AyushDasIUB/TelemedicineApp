9ecd8f671bc20bd8c6529a06b6b78a62340d4ee1
Ayush Das <dasayush953@gmail.com>
2024-04-12 23:41:47 +0600

    Adding file
    Adding all file that I was working on

---
+++ Group52_TelemedicineApp_2231523_2211243_2220363_2230160/src/Ayush/FXMLNurseDashboardSceneController.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Ayush;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author dasay
+ */
+public class FXMLNurseDashboardSceneController implements Initializable {
+
+    
+    @FXML
+    private BorderPane mainBorderPane;
+    @FXML
+    private AnchorPane welcomeAnchorePane;
+    @FXML
+    private Label nurseNameInWelcomePage;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        mainBorderPane.setCenter(welcomeAnchorePane);
+        
+    }    
+
+    @FXML
+    private void profileButtonOnAction(ActionEvent event) {
+        loadPage("FXMLNurseProfileScene");
+        
+    }
+
+    @FXML
+    private void findPatientsOnAction(ActionEvent event) {
+        loadPage("FXMLNurseFindPatientScene");
+    }
+
+    @FXML
+    private void schedulesButtonOnAction(ActionEvent event) {
+        loadPage("FXMLNurseSchedulesScene");
+    }
+
+    @FXML
+    private void assignedPatientButtonOnAction(ActionEvent event) {
+        loadPage("FXMLNurseAssignedPatientsScene");
+    }
+
+    @FXML
+    private void trainingButtonOnAction(ActionEvent event) {
+        loadPage("FXMLNurseTraining&CertificationsScene");
+    }
+
+    @FXML
+    private void exitButtonOnAction(ActionEvent event) {
+        System.exit(0);
+    }
+    
+    private void loadPage(String page){
+        Parent root = null;
+        try{
+          root = FXMLLoader.load(getClass().getResource(page+".fxml"));
+        }catch(IOException ex){
+              Logger.getLogger(FXMLNurseDashboardSceneController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        mainBorderPane.setCenter(root);      
+    }
+
+    void initData(String token) {
+        nurseNameInWelcomePage.setText(token);
+    }
+
+ 
+    
+}
--- Group52_TelemedicineApp_2231523_2211243_2220363_2230160/src/Ayush/MainApplication.java
+++ Group52_TelemedicineApp_2231523_2211243_2220363_2230160/src/Ayush/MainApplication.java
@@ -18,7 +18,7 @@ public class MainApplication extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLLoginScene.fxml"));
         
         Scene scene = new Scene(root);
         
