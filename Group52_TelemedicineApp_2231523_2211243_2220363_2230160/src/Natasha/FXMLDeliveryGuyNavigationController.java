b9cf44c62fe237800db33192409590e47fdc8b11
Ayush Das <dasayush953@gmail.com>
2024-04-16 00:44:30 +0600

    push

--- Group52_TelemedicineApp_2231523_2211243_2220363_2230160/src/Natasha/FXMLDeliveryGuyNavigationController.java
+++ Group52_TelemedicineApp_2231523_2211243_2220363_2230160/src/Natasha/FXMLDeliveryGuyNavigationController.java
@@ -37,24 +37,9 @@ public class FXMLDeliveryGuyNavigationController implements Initializable {
 
     @FXML
     private void navigationButton(ActionEvent event) {
-        /*/* public class /*DeliveryInfo*/ */{*/
-            /*public static void main(String[] args) {*/
-        // Define the delivery information in a 2D array
-        String[][] deliveryInfo = {
-                {"Customer Name", "Delivery Address", "Distance", "Duration"},
-                {"Alice", "Dhaka", "100km", "1 hour"},
-                {"Bob", "Dhaka", "150km", "2 hours"}
-        };
 
-        // Print the delivery information in a table format
-        for (int i = 0; i < deliveryInfo.length; i++) {
-            for (int j = 0; j < deliveryInfo[i].length; j++) {
-                System.out.printf("%-15s", deliveryInfo[i][j]); // Adjust the width as needed
-            }
-            System.out.println();
-        }
-    }
-}
+
+
 
     }
     
