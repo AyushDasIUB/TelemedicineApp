065f67ed4ca1fc4e27783864416cf1d03af6b8e3
Ayush Das <dasayush953@gmail.com>
2024-04-16 02:19:13 +0600

    Edit table in Orin

---
+++ Group52_TelemedicineApp_2231523_2211243_2220363_2230160/src/Natasha/CheckStock.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Natasha;
+
+/**
+ *
+ * @author dasay
+ */
+public class CheckStock {
+    protected String name;
+    protected String que;
+    protected String loc;
+
+    public CheckStock(String name, String que, String loc) {
+        this.name = name;
+        this.que = que;
+        this.loc = loc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQue() {
+        return que;
+    }
+
+    public void setQue(String que) {
+        this.que = que;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Group52_TelemedicineApp_2231523_2211243_2220363_2230160/src/Natasha/FSchedule.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Natasha;
+
+/**
+ *
+ * @author dasay
+ */
+public class FSchedule {
+    protected String day;
+    protected String time;
+
+    public FSchedule(String day, String time) {
+        this.day = day;
+        this.time = time;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+    
+    
+}
